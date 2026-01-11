cad32fae2bf0fac49988f96f358597946e10a5fa
Afros Rajabov <afrosrajabov1207@mail.ru>
2026-01-12 03:40:13 +0700

    uninstall script

--- install.sh
+++ install.sh
@@ -2,8 +2,10 @@
 
 # NOTE: Script for installing all other scripts by creating symlinks
 
+WHERE=~/.local/bin
+
 # Query all sh scripts except for current one
-scripts=$(find . -name "*.sh" ! -name "install.sh" -type f -printf "%P\n")
+scripts=$(find . -name "*.sh" ! -name "*install.sh" -type f -printf "%P\n")
 
 # Create a list
 files=()
@@ -16,7 +18,15 @@ done <<<"$scripts"
 # Do the thing
 for script in "${files[@]}"; do
 	script_name="${script%.sh}"
+	target="$(pwd)/$script"
+	destination="${WHERE}/${script_name}"
+
+	if [[ -f $destination ]]; then
+		printf "$script_name already exists. Skipping...\n"
+		continue
+	fi
+
 	chmod +x $script
-	ln -s "$(pwd)/$script" ~/.local/bin/"${script_name}"
+	ln -s $target $destination
 	printf "${script_name} is installed\n"
 done
---
+++ uninstall.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# TODO: Script for uninstalling all the scripts
+
+WHERE=~/.local/bin
+
+# Query all sh scripts except for current one
+scripts=$(find . -name "*.sh" ! -name "*install.sh" -type f -printf "%P\n")
+
+# Create a list
+script_names=()
+while IFS= read -r line; do
+	if [[ $line == *.sh* ]]; then
+		script_name="${line%.sh}"
+		target="${WHERE}/${script_name}"
+
+		if [[ ! -f $target ]]; then
+			printf "$script_name doesn't exists. Skipping...\n"
+			continue
+		fi
+
+		rm $target
+	fi
+done <<<"$scripts"
